52a0328334559e5104f40c6e7e96e8a1885e0f7f
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2020-12-05 10:14:00 +0100

    Prepend (instead of append) the node to the path
    node <- path[1L]
    
    is a bit more elegant than
    
    node <- path[length(path)]
    
    This requires to reverse the path with rev()
    when being output

--- haus-vom-nikolaus.R
+++ haus-vom-nikolaus.R
@@ -15,12 +15,12 @@ edges <- c("12", "14", "15", "23", "24", "25", "34", "45")
 go <- function(path, seen = logical(length(edges))) {  
   
   if (length(path) == 9L) {
-    writeLines(paste(path, collapse = "-"))
+    writeLines(paste(rev(path), collapse = "-"))
     return()
   }
   
-  for (edge in grep(node <- path[length(path)], edges[! seen], value = TRUE))
-    go(c(path, gsub(node, "", edge)), `[<-`(seen, edges == edge, TRUE))
+  for (edge in grep(node <- path[1L], edges[! seen], value = TRUE))
+    go(c(gsub(node, "", edge), path), `[<-`(seen, edges == edge, TRUE))
 }
 
 result <- capture.output(for (i in 1:5) go(i))
