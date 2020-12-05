493ab6addec6bfebe0a6eaa6588c03341052f1f1
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2020-12-05 09:05:33 +0100

    Rename "a" to "node" and "link(s)" to "edge(s)"
    These are the terms used in graph theory.

--- haus-vom-nikolaus.R
+++ haus-vom-nikolaus.R
@@ -10,19 +10,19 @@
 #  | /   \ |
 # (1)-----(5)
 
-links <- c("12", "14", "15", "23", "24", "25", "34", "45")
+edges <- c("12", "14", "15", "23", "24", "25", "34", "45")
 
-go <- function(path, seen = logical(length(links))) {  
+go <- function(path, seen = logical(length(edges))) {  
   
   if (length(path) == 9L) {
     cat(paste(path, collapse = "-"), "\n")
     return()
   }
   
-  a <- path[length(path)]
+  node <- path[length(path)]
   
-  for (link in grep(a, links[! seen], value = TRUE)) {
-    go(c(path, gsub(a, "", link)), `[<-`(seen, links == link, TRUE))
+  for (edge in grep(node, edges[! seen], value = TRUE)) {
+    go(c(path, gsub(node, "", edge)), `[<-`(seen, edges == edge, TRUE))
   }
 }
 
